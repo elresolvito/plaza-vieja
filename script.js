58d62c91a8973a2c45d6e7b26278cd6b37e91bd7
jccarlos010277-cmd <jccarlos010277@gmail.com>
2026-08-30 11:02:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -508,7 +508,7 @@ function obtenerMensajePedido() {
     mensaje += '\n📞 *Teléfono:* ' + telefonoCliente;
     mensaje += '\n💳 *Pago:* (Efectivo / Transferencia)';
     mensaje += '\n\n⏰ *Entregamos en el día (9am - 8pm)*';
-    mensaje += '\n\n✅ *¡Gracias por tu compra!*';
+    mensaje += '\n\n✅ *El administrador confirmará los datos y coordinará la entrega*';
 
     return mensaje;
 }
